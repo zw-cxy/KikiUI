f69866a6b0447b3d1d5d137a49cbd708719ec776
zhzwei <491239937@qq.com>
2021-03-09 23:37:14 +0800

    button

---
+++ kiki-ui/components/ki-button.vue
@@ -0,0 +1,36 @@
+<template>
+  <view>
+    <button class="ki-btn ki-btn_primary">页面主操作</button>
+    <button class="ki-btn ki-btn_primary ki-btn_loading">
+      <text class="ki-loading-primary ki-loading-primary_transparent">
+        <text class="ki-loading-primary__dot"></text>
+      </text>
+      <text>页面主操作</text>
+    </button>
+  </view>
+</template>
+
+<script>
+/**
+ * 
+ * @description
+ * 
+ */
+export default {
+  props: {
+    
+  },
+  data() {
+    return {
+      
+    }
+  },
+  methods: {
+    
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ kiki-ui/examples/button.vue
@@ -0,0 +1,19 @@
+<template>
+  <view>
+    <ki-button></ki-button>
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
